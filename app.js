1ca4bd939767d90e142d633b02534c0d62ee0807
dhtjddls <dhtjddls12@naver.com>
2023-04-18 20:51:14 +0900

    feat: Post & Comment Router, auth-middleware

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-
-const postsRouter = require('./routes/posts.js');
-const commentsRouter = require('./routes/comments.js');
-const connect = require('./schemas');
+const cookieParser = require("cookie-parser");
+const postsRouter = require("./routes/posts.js");
+const commentsRouter = require("./routes/comments.js");
+const authRouter = require("./routes/auth.js");
+const connect = require("./schemas");
 connect();
 
 app.use(express.json());
-
-app.use("/", [postsRouter, commentsRouter]);
+app.use(cookieParser());
+app.use("/", [authRouter]);
+app.use("/posts", [postsRouter, commentsRouter]);
 
 app.listen(port, () => {
-    console.log('Server open on port', port);
+  console.log("Server open on port", port);
 });
